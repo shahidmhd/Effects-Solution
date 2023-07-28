ce295ca3c3f22c9979b2cfcccf56507069e75edb
shahidmhd <shahidvk124@gmail.com>
2023-07-28 11:30:57 +0530

    user side listed

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -51,5 +51,61 @@ module.exports = {
         } catch (err) {
             console.log(err);
         }
+    },
+    AppDevelopment: async (req, res) => {
+        try {
+            res.render('user/AppDevelopment')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    Digitalmarketing: async (req, res) => {
+        try {
+            res.render('user/Digitalmarketing')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    BuisnessConsulting: async (req, res) => {
+        try {
+            res.render('user/BuisnessConsulting')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    WebAnalitic: async (req, res) => {
+        try {
+            res.render('user/WebAnalitics')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+     Ecommerce: async (req, res) => {
+        try {
+            res.render('user/Ecommerce')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    EnterPrice: async (req, res) => {
+        try {
+            res.render('user/EnterPriceDevelopment')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    itstaf: async (req, res) => {
+        try {
+            res.render('user/itstaf')
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    branding: async (req, res) => {
+        try {
+            res.render('user/Branding&Graphics')
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ router.get('/products',usercontroller.userproduct)
 router.get('/softwareDevelopment',usercontroller.softwareDevelopment)
 router.get('/webDevelopment',usercontroller.webDevelopment)
 router.get('/mobiledevelopment',usercontroller.mobileDevelopment)
+router.get('/Appdevelopment',usercontroller.AppDevelopment)
+router.get('/Digitalmarketing',usercontroller.Digitalmarketing)
+router.get('/BuisnessConsulting',usercontroller.BuisnessConsulting)
+router.get('/WebAnalitics',usercontroller.WebAnalitic)
+router.get('/Ecommerce',usercontroller.Ecommerce)
+router.get('/EnterPrice',usercontroller.EnterPrice)
+router.get('/itstaf',usercontroller.itstaf)
+router.get('/branding',usercontroller.branding)
 
 
 
